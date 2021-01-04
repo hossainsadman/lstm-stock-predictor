634c5d1cce75a3e9435d41ccfea50b96f3132025
Sadman Hossain <m.sadman.h@gmail.com>
2021-01-03 22:59:20 -0500

    Add graph creator function

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import urllib.request
 import json
-import pandas
+import pandas as pd
 import datetime
 
 load_dotenv()
@@ -16,10 +16,12 @@ if not os.path.exists(data_file):
     data = json.loads(urllib.request.urlopen(url).read().decode())
     data = data['Time Series (Daily)']
 
-    df = pandas.DataFrame(columns=['Date','Low','High','Close','Open'])
+    df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
     for i, j in data.items():
         date = datetime.datetime.strptime(i, '%Y-%m-%d')
         data_row = [date.date(), float(j['3. low']), float(j['2. high']), float(j['4. close']), float(j['1. open'])]
         df.loc[-1,:] = data_row
         df.index += 1
     df.to_csv(data_file, index=False)
+
+data = pd.read_csv(data_file)
---
+++ images.py
@@ -0,0 +1,12 @@
+from data import data, TICKER
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style("darkgrid")
+plt.figure(figsize = (15,9))
+plt.plot(data[['Close']])
+plt.xticks(range(0,data.shape[0],500),data['Date'].loc[::500],rotation=45)
+plt.title("Qualcomm Stock Price",fontsize=18, fontweight='bold')
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Close (USD)',fontsize=18)
+plt.savefig(TICKER + ' Stock Price')
