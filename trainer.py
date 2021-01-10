b640120a0183655acb2e0a5bc4281c7b38a4ca6d
Sadman Hossain <m.sadman.h@gmail.com>
2021-01-09 23:04:46 -0500

    Create LSTM class

--- trainer.py
+++ trainer.py
@@ -43,3 +43,25 @@ x_train = torch.from_numpy(x_train).type(torch.Tensor)
 x_test = torch.from_numpy(x_test).type(torch.Tensor)
 y_train_lstm = torch.from_numpy(y_train).type(torch.Tensor)
 y_test_lstm = torch.from_numpy(y_test).type(torch.Tensor)
+
+input_dim = 1
+hidden_dim = 32
+num_layers = 2
+output_dim = 1
+num_epochs = 100
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:, -1, :])
+        return out
