ca560d00e384aafe001f5b2d462635a7fc8744b0
Sadman Hossain <m.sadman.h@gmail.com>
2021-01-11 23:10:18 -0500

    Create graphs for prediction and training loss

--- trainer.py
+++ trainer.py
@@ -95,3 +95,25 @@ for t in range(num_epochs):
 
 training_time = time.time()-start_time
 print("Training time: {}".format(training_time))
+
+predict = pd.DataFrame(scaler.inverse_transform(y_train_pred.detach().numpy()))
+original = pd.DataFrame(scaler.inverse_transform(y_train_lstm.detach().numpy()))
+
+fig = plt.figure()
+fig.subplots_adjust(hspace=0.2, wspace=0.2)
+
+plt.subplot(1, 2, 1)
+ax = sns.lineplot(x = original.index, y = original[0], label="Data", color='royalblue')
+ax = sns.lineplot(x = predict.index, y = predict[0], label="Training Prediction (LSTM)", color='tomato')
+ax.set_title('Stock price', size = 14, fontweight='bold')
+ax.set_xlabel("Days", size = 14)
+ax.set_ylabel("Cost (USD)", size = 14)
+ax.set_xticklabels('', size=10)
+
+plt.subplot(1, 2, 2)
+ax = sns.lineplot(data=hist, color='royalblue')
+ax.set_xlabel("Epoch", size = 14)
+ax.set_ylabel("Loss", size = 14)
+ax.set_title("Training Loss", size = 14, fontweight='bold')
+fig.set_figheight(6)
+fig.set_figwidth(16)
